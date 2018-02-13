25fe99502b9db7210d0343126c9aaaf46789ab67
Brian Douglass <bhdouglass@gmail.com>
2018-02-12 23:36:31 -0500

    Fixed redirect

--- packages/openstore-api/src/app/index.js
+++ packages/openstore-api/src/app/index.js
@@ -26,9 +26,7 @@ function setup() {
 
     app.use(function(req, res, next) {
         let host = config.server.host.replace('https://', '').replace('http://', '');
-        let secondary_host = config.server.host.replace('https://', '').replace('http://', '');
-
-        console.log(req.headers.host, host, secondary_host);
+        let secondary_host = config.server.secondary_host.replace('https://', '').replace('http://', '');
 
         if (req.headers.host != host && req.headers.host != secondary_host) {
             res.redirect(301, config.server.host + req.originalUrl);
