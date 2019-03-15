5b12a4cde4dc89dd64265e99140dbc544b1b926c
Brian Douglass <bhdouglass@gmail.com>
2019-03-15 10:05:16 -0400

    Refactored rss

--- packages/openstore-api/src/api/rss.js
+++ packages/openstore-api/src/api/rss.js
@@ -2,6 +2,7 @@ const RSS = require('rss');
 const express = require('express');
 
 const Package = require('../db/package/model');
+const PackageRepo = require('../db/package/repo');
 const logger = require('../utils/logger');
 const config = require('../utils/config');
 const packages = require('../utils/packages');
@@ -9,64 +10,29 @@ const helpers = require('../utils/helpers');
 
 const router = express.Router();
 
-router.get('/new.xml', async (req, res) => {
+async function generateFeed(res, updates) {
     let feed = new RSS({
-        title: 'New Apps in the OpenStore',
-        description: 'The hottest new apps for Ubuntu Touch',
-        feed_url: `${config.server.host}/rss/new.xml`,
+        title: updates ? 'Updated Apps in the OpenStore' : 'New Apps in the OpenStore',
+        description: updates ? 'Cool updates for Ubuntu Touch apps' : 'The hottest new apps for Ubuntu Touch',
+        feed_url: `${config.server.host}/rss/${updates ? 'updates' : 'new'}.xml`,
         site_url: config.server.host,
         image_url: `${config.server.host}/logo.png`,
         ttl: 240, // 4 hours
     });
 
     try {
-        let pkgs = await Package.find({
-            published: true,
-        }).sort('-published_date').limit(10).exec();
+        let sort = updates ? '-updated_date' : '-published_date';
+        let pkgs = await PackageRepo.find({published: true}, sort, 10);
 
         pkgs.forEach((pkg) => {
+            let changelog = '';
             let description = pkg.description ? `<br/>${pkg.description}` : '';
-            let url = `${config.server.host}/app/${pkg.id}`;
+            if (updates) {
+                changelog = pkg.changelog ? `<br/><br/>Changelog:<br/>${pkg.changelog}` : '';
+                changelog = changelog.replace('\n', '<br/>');
+                description = pkg.description ? `<br/><br/>Description:<br/>${pkg.description}` : '';
+            }
 
-            feed.item({
-                title: pkg.name,
-                url: url,
-                description: `<a href="${url}"><img src="${packages.iconUrl(pkg)}" /></a>${description}`,
-                author: pkg.author,
-                date: pkg.updated_date,
-                custom_elements: [{tagline: pkg.tagline ? pkg.tagline : ''}],
-            });
-        });
-    }
-    catch (err) {
-        logger.error('RSS feed error', err);
-        helpers.error(res, 'There was an error generating the RSS feed');
-        return;
-    }
-
-    res.header('Content-Type', 'text/xml');
-    res.send(feed.xml({indent: true}));
-});
-
-router.get('/updates.xml', async (req, res) => {
-    let feed = new RSS({
-        title: 'Updated Apps in the OpenStore',
-        description: 'Cool updates for Ubuntu Touch apps',
-        feed_url: `${config.server.host}/rss/updates.xml`,
-        site_url: config.server.host,
-        image_url: `${config.server.host}/logo.png`,
-        ttl: 240, // 4 hours
-    });
-
-    try {
-        let pkgs = await Package.find({
-            published: true,
-        }).sort('-updated_date').limit(10).exec();
-
-        pkgs.forEach((pkg) => {
-            let changelog = pkg.changelog ? `<br/><br/>Changelog:<br/>${pkg.changelog}` : '';
-            changelog = changelog.replace('\n', '<br/>');
-            let description = pkg.description ? `<br/><br/>Description:<br/>${pkg.description}` : '';
             let url = `${config.server.host}/app/${pkg.id}`;
 
             feed.item({
@@ -81,12 +47,19 @@ router.get('/updates.xml', async (req, res) => {
     }
     catch (err) {
         logger.error('RSS feed error', err);
-        helpers.error(res, 'There was an error generating the RSS feed');
-        return;
+        return helpers.error(res, 'There was an error generating the RSS feed');
     }
 
     res.header('Content-Type', 'text/xml');
     res.send(feed.xml({indent: true}));
+}
+
+router.get('/new.xml', (req, res) => {
+    generateFeed(res, false);
+});
+
+router.get('/updates.xml', async (req, res) => {
+    generateFeed(res, true);
 });
 
 module.exports = router;
