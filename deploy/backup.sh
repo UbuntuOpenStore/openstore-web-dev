641078ecb7dd97231bfa33de7636a41c51a779ea
Brian Douglass <bhdouglass@gmail.com>
2020-04-17 00:00:13 -0400

    Added backup script, removed backblaze config, updated compose

---
+++ deploy/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /srv/openstore/env.sh
+
+set -x
+set -e
+
+b2 sync --noProgress --delete $DATA_DIR b2://openstore-backup/data
+b2 sync --noProgress --delete $IMAGE_DIR b2://openstore-backup/images
+b2 sync --noProgress --delete $ICON_DIR b2://openstore-backup/icons
+
+#mongodump -h $MONGODB_HOST -d $MONGODB_DB -u $MONGODB_USER -p $MONGODB_PASSWORD --collection=packages -o /tmp/mongo-backup
+#b2 sync --noProgress --delete /tmp/mongo-backup b2://openstore-backup/mongo
