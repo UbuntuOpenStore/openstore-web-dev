8c4e1a9e4cf7609cd088be98f79d7d35908bdd41
Brian Douglass <bhdouglass@gmail.com>
2020-11-29 23:47:53 -0500

    Updated backup script, base image, and volumes

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -11,9 +11,8 @@ b2 sync --noProgress --delete $DATA_DIR b2://openstore-backup/data
 b2 sync --noProgress --delete $IMAGE_DIR b2://openstore-backup/images
 b2 sync --noProgress --delete $ICON_DIR b2://openstore-backup/icons
 
-mongodump -h $MONGODB_HOST -d $MONGODB_DB -u $MONGODB_USER -p $MONGODB_PASSWORD \
-    --collection=packages \
-    --collection=ratingcounts \
-    --collection=reviews \
-    -o /tmp/mongo-backup
-b2 sync --noProgress --delete /tmp/mongo-backup b2://openstore-backup/mongo
+mongodump -h $MONGODB_HOST -d $MONGODB_DB -o /srv/openstore/backup-full/
+mongodump -h $MONGODB_HOST -d $MONGODB_DB --collection=packages -o /srv/openstore/backup/
+mongodump -h $MONGODB_HOST -d $MONGODB_DB --collection=ratingcounts -o /srv/openstore/backup/
+mongodump -h $MONGODB_HOST -d $MONGODB_DB --collection=reviews -o /srv/openstore/backup/
+b2 sync --noProgress --delete /srv/openstore/backup/ b2://openstore-backup/mongo
