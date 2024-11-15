384f061dbb8f181dcd2c6329c87ea62b90aaf9fb
Brian Douglass <bhdouglass@gmail.com>
2024-11-15 09:24:18 -0600

    Added deployment

---
+++ packages/openstore-web-next/post-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VERSION=$1
+ENV=${2:-""}
+BASE=/srv/openstore-web-next$ENV
+
+set -x
+set -e
+
+cd $BASE/$VERSION
+
+rm -f $BASE/current
+ln -s $BASE/$VERSION $BASE/current
+
+sudo systemctl restart openstore-web-next$ENV
+
+cd $BASE/
+echo "Going to remove old versions"
+ls -1t | grep -v current | tail -n +10
+ls -1t | grep -v current | tail -n +10 | xargs -d '\n' -r rm -r --
