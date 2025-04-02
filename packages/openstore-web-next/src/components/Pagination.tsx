5dbbdcfb2abc1f82cc340d6952d37be4b0282ec8
Brian Douglass <bhdouglass@gmail.com>
2025-04-01 23:06:19 -0400

    Added pagination to the manage app list

--- packages/openstore-web-next/src/components/Pagination.tsx
+++ packages/openstore-web-next/src/components/Pagination.tsx
@@ -18,21 +18,21 @@ type Props = {
 const Pagination = ({ currentPage, totalPages, onPageChanged }: Props) => {
   return (
     <div class="flex gap-4 justify-center">
-      <button disabled={currentPage <= 0} class={`${currentPage <= 0 ? 'text-gray-400' : ''}`} onClick={() => onPageChanged(currentPage - 1)} title="Go back a page">
+      <button disabled={currentPage <= 0} class={`${currentPage <= 0 ? 'text-gray-400' : 'cursor-pointer'}`} onClick={() => onPageChanged(currentPage - 1)} title="Go back a page">
         <SvgGoPrevious />
       </button>
 
       {currentPage >= 3 && (<>...</>)}
 
-      {currentPage >= 2 && (<button class="underline" title={buttonTitle(currentPage - 2)} onClick={() => onPageChanged(currentPage - 2)}>{formatPage(currentPage - 2)}</button>)}
+      {currentPage >= 2 && (<button class="underline cursor-pointer" title={buttonTitle(currentPage - 2)} onClick={() => onPageChanged(currentPage - 2)}>{formatPage(currentPage - 2)}</button>)}
       {currentPage >= 1 && (<button class="underline" title={buttonTitle(currentPage - 1)} onClick={() => onPageChanged(currentPage - 1)}>{formatPage(currentPage - 1)}</button>)}
-      <button class="underline" title={buttonTitle(currentPage)} onClick={() => onPageChanged(currentPage)}>{formatPage(currentPage)}</button>
-      {currentPage >= 0 && (totalPages - currentPage) >= 1 && (<button class="underline" title={buttonTitle(currentPage + 1)} onClick={() => onPageChanged(currentPage + 1)}>{formatPage(currentPage + 1)}</button>)}
-      {currentPage >= 0 && (totalPages - currentPage) >= 2 && (<button class="underline" title={buttonTitle(currentPage + 2)} onClick={() => onPageChanged(currentPage + 2)}>{formatPage(currentPage + 2)}</button>)}
+      <button class="underline cursor-pointer font-bold" title={buttonTitle(currentPage)} onClick={() => onPageChanged(currentPage)}>{formatPage(currentPage)}</button>
+      {currentPage >= 0 && (totalPages - currentPage) >= 1 && (<button class="underline cursor-pointer" title={buttonTitle(currentPage + 1)} onClick={() => onPageChanged(currentPage + 1)}>{formatPage(currentPage + 1)}</button>)}
+      {currentPage >= 0 && (totalPages - currentPage) >= 2 && (<button class="underline cursor-pointer" title={buttonTitle(currentPage + 2)} onClick={() => onPageChanged(currentPage + 2)}>{formatPage(currentPage + 2)}</button>)}
 
       {currentPage >= 0 && (totalPages - currentPage) >= 3 && (<span class="text-gray-400">...</span>)}
 
-      <button disabled={currentPage >= totalPages} class={`${currentPage >= totalPages ? 'text-gray-400' : ''}`} onClick={() => onPageChanged(currentPage + 1)} title="Go to the next page">
+      <button disabled={currentPage >= totalPages} class={`${currentPage >= totalPages ? 'text-gray-400' : 'cursor-pointer'}`} onClick={() => onPageChanged(currentPage + 1)} title="Go to the next page">
         <SvgGoNext />
       </button>
     </div>
---
+++ packages/openstore-web-next/src/components/StaticPagination.tsx
@@ -0,0 +1,50 @@
+import SvgGoNext from "./icons/GoNext";
+import SvgGoPrevious from "./icons/GoPrevious";
+
+function formatPage(page: number) {
+  return page + 1;
+}
+
+function buttonTitle(page: number) {
+  return `Go to page ${formatPage(page)}`;
+}
+
+type Props = {
+  currentPage: number;
+  totalPages: number;
+  baseUrl: string;
+};
+
+const StaticPagination = ({ currentPage, totalPages, baseUrl }: Props) => {
+  function makeUrl(page: number) {
+    if (baseUrl.includes('?')) {
+      return `${baseUrl}&page=${page}`;
+    }
+
+    return `${baseUrl}?page=${page}`;
+  }
+
+  return (
+    <div class="flex gap-4 justify-center">
+      <a disabled={currentPage <= 0} class={`${currentPage <= 0 ? 'text-gray-400' : 'cursor-pointer'}`} href={makeUrl(currentPage - 1)} title="Go back a page">
+        <SvgGoPrevious />
+      </a>
+
+      {currentPage >= 3 && (<>...</>)}
+
+      {currentPage >= 2 && (<a class="underline cursor-pointer" title={buttonTitle(currentPage - 2)} href={makeUrl(currentPage - 2)}>{formatPage(currentPage - 2)}</a>)}
+      {currentPage >= 1 && (<a class="underline" title={buttonTitle(currentPage - 1)} href={makeUrl(currentPage - 1)}>{formatPage(currentPage - 1)}</a>)}
+      <a class="underline cursor-pointer font-bold" title={buttonTitle(currentPage)} href={makeUrl(currentPage)}>{formatPage(currentPage)}</a>
+      {currentPage >= 0 && (totalPages - currentPage) >= 1 && (<a class="underline cursor-pointer" title={buttonTitle(currentPage + 1)} href={makeUrl(currentPage + 1)}>{formatPage(currentPage + 1)}</a>)}
+      {currentPage >= 0 && (totalPages - currentPage) >= 2 && (<a class="underline cursor-pointer" title={buttonTitle(currentPage + 2)} href={makeUrl(currentPage + 2)}>{formatPage(currentPage + 2)}</a>)}
+
+      {currentPage >= 0 && (totalPages - currentPage) >= 3 && (<span class="text-gray-400">...</span>)}
+
+      <a disabled={currentPage >= totalPages} class={`${currentPage >= totalPages ? 'text-gray-400' : 'cursor-pointer'}`} href={makeUrl(currentPage + 1)} title="Go to the next page">
+        <SvgGoNext />
+      </a>
+    </div>
+  );
+};
+
+export default StaticPagination;
