27fe26b23070eb872a4385ee867355ccf907da01
Brian Douglass <bhdouglass@gmail.com>
2025-04-09 16:05:01 -0400

    Improved mobile experience for the app list

--- packages/openstore-web-next/src/components/apps/AppList.tsx
+++ packages/openstore-web-next/src/components/apps/AppList.tsx
@@ -5,17 +5,25 @@ import { getRelativeLocaleUrl } from "@/lib/utils";
 
 const AppList = ({ apps, messages, currentLocale }: { apps: SlimAppData[], currentLocale: string | undefined, messages: { new: string, app: string, bookmark: string, webapp: string } }) => {
   return (
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+    <div class="grid grid-cols-2 md:grid-cols-4">
       {
         apps.map((app) => (
-          <div class="flex flex-row gap-4 max-w-xl mb-4 p-4">
+          <div class="flex flex-row gap-4 max-w-xl mb-4 py-2 px-1 md:p-4">
             <div>
               <a href={getRelativeLocaleUrl(currentLocale, `/app/${app.id}/`)}>
-                <img class="rounded-2xl" src={app.icon} alt={app.name} width="64" height="64" loading="eager" style={`view-transition-name: app-${app.id.replace(/\./g, '-')}`} />
+                <img
+                  class="rounded-2xl max-w-[64px] max-h-[64px]"
+                  src={app.icon}
+                  alt={app.name}
+                  width="64"
+                  height="64"
+                  loading="eager"
+                  style={`view-transition-name: app-${app.id.replace(/\./g, '-')}`}
+                />
               </a>
             </div>
             <div>
-              <h1 class="text-lg">
+              <h1 class="text-md md:text-lg text-ellipsis line-clamp-2">
                 <a href={getRelativeLocaleUrl(currentLocale, `/app/${app.id}/`)} class="underline">
                   {app.name}
                 </a>
